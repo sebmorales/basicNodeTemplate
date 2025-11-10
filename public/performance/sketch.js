59ee8b09fca5cfe39f4dc5cde25080c68be899ff
seb <sebmoralesprado@gmail.com>
2025-11-10 08:39:42 -0500

    fixed typos

--- public/performance/sketch.js
+++ public/performance/sketch.js
@@ -2,7 +2,7 @@ let socket = io();
 let images_src=["bug.jpg","cat.gif","giphy.gif","moodeng.gif","moodeng2.gif","thumbnails_computer.jpg","thumbnails_forest.jpg","thumbnails_subway.jpg","thumbnails_taxi.jpg","thumbnails_underground.jpg"]
 let image_folder="/images/"
 let images=[]
-let sounds_src=["bubble1.wav","bubble2.wav","bubble3.wav","bubble4.wav","bubble5.wav","bubble6.wav","bubble7.wav","click.wav","click2.wav","click3.wav","click4.wav","click5.wav","crackling1.wav","crackling2.wav","crackling3.wav","crackling4.wave","pop.wav","bird1.mp3"]
+let sounds_src=["bubble1.wav","bubble2.wav","bubble3.wav","bubble4.wav","bubble5.wav","bubble6.wav","bubble7.wav","click.wav","click2.wav","click3.wav","click4.wav","click5.wav","cracking1.wav","cracking2.wav","cracking3.wav","cracking4.wav","pop.wav","bird1.mp3"]
 let sounds_folder="/sounds/"
 let sounds=[]
 let current_image;
